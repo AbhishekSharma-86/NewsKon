1d244625495d8c24a35755264e3b492a52acd724
Abhishek Sharma <abhisheksharma86490@gmail.com>
2021-07-16 12:26:14 +0530

    Adding more categories

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
-function news(){var request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
+
 request.onload = function () {
     let response = JSON.parse(request.response);
     let articles = response.articles;
     let newshtml = "";
+
     let bennernews = `<div class="box-main">
                             <div class="title-section">
                                 <div class="first-half">${articles[0].title}</div>
@@ -15,31 +17,49 @@ request.onload = function () {
     document.querySelector(".banner").innerHTML = bennernews;
     articles.forEach(element => {
         let news = `<div class="container" style="display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 70%;
-        background-color: azure;
-        margin: 3% 0;">
-                        <img src="${element.urlToImage}" class="news-img" alt="Image" style="width: 50%;margin: 2% 0;">
-                        <div class="news-title">
-                            <h3 class="heading" style="font-size: 2em;
-                            font-family: 'Lato', sans-serif;
-                            padding: 0 10%;
-                            margin-bottom: 2%">${element.title}</h3>
-                        </div>
-                        <div class="news-description" style="padding: 0 2%;
-                        margin-bottom: 2%;">
-                            <p>${element.description} <a href="${element.url}" target="_blank">read more</a></p>
-                        </div>
-                    </div>`;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 70%;
+            background-color: azure;
+            margin: 3% 0;">
+            <img src="${element.urlToImage}" class="news-img" alt="Image" style="width: 50%;margin: 2% 0;">
+            <div class="news-title">
+                <h3 class="heading" style="font-size: 2em;
+                font-family: 'Lato', sans-serif;
+                padding: 0 10%;
+                margin-bottom: 2%">${element.title}</h3>
+            </div>
+            <div class="news-description" style="padding: 0 2%;
+            margin-bottom: 2%;">
+                <p>${element.description} <a href="${element.url}" target="_blank">read more</a></p>
+            </div>
+        </div>`;
         newshtml += news;
     });
     document.querySelector("#news-content").innerHTML = newshtml;
 }
-request.open('get', 'https://newsapi.org/v2/top-headlines?country=in&apiKey=4d9c5b5dac014dd49d63889d8f26490f');
-request.send();
+var contenturl = "country=in";
+if (document.title.includes("Covid")){
+    contenturl = "q=covid&country=in";
 }
-news();
+else if(document.title.includes("Technical")){
+    contenturl = "country=in&category=technology";
+}else if(document.title.includes("Politics")){
+    contenturl = "country=in&q=political";
+}
+    // covid.addEventListener("click", function(e){
+    //     // e.preventDefault();
+    //     console.log("I am annoying you");
+    //     contenturl = "q=covid&country=in";
+    //     return false;
+    // }); 
+    // covid.onclick = function(){
+    //     contenturl = "q=covid&country=in";
+    // };
+    let apiURL = `https://newsapi.org/v2/top-headlines?${contenturl}&apiKey=4d9c5b5dac014dd49d63889d8f26490f`;
+request.open('get', apiURL);
+request.send();
+
 
 
